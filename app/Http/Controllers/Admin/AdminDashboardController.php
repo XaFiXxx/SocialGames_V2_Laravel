9c8d2572745b994020a57751998771d116657bc0
Benjamin <benjamin.schroeders@icloud.com>
2026-04-20 00:52:50 +0200

    Améliorations des stats de bases sur la page home

--- app/Http/Controllers/Admin/AdminDashboardController.php
+++ app/Http/Controllers/Admin/AdminDashboardController.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Game;
+use App\Models\Genre;
 use App\Models\Message;
+use App\Models\Platform;
 use App\Models\Post;
 use App\Models\User;
 use Illuminate\Http\JsonResponse;
@@ -16,7 +19,12 @@ class AdminDashboardController extends Controller
             'users' => User::count(),
             'posts' => Post::count(),
             'messages' => Message::count(),
+            'games' => Game::count(),
+            'platforms' => Platform::count(),
+            'genres' => Genre::count(),
+
             'new_users_24h' => User::where('created_at', '>=', now()->subDay())->count(),
+            'new_posts_24h' => Post::where('created_at', '>=', now()->subDay())->count(),
         ]);
     }
 }
