809128e72f24d6e2b69ee98eec43c0ae10a52cc2
Benjamin <benjamin.schroeders@icloud.com>
2026-04-22 03:36:32 +0200

    VPS #3

--- config/cors.php
+++ config/cors.php
@@ -14,7 +14,9 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:5173'],
+    'allowed_origins' => array_filter(
+    explode(',', (string) env('CORS_ALLOWED_ORIGINS'))
+    ),
 
     'allowed_origins_patterns' => [],
 
